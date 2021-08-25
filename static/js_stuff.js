c104eb5561ed93ec7a8431021d18913beb780987
Lucy Hao <hao.lucyy@gmail.com>
2021-08-25 09:16:12 -0600

    change expand collaspe for experiences

--- static/js_stuff.js
+++ static/js_stuff.js
@@ -40,3 +40,18 @@ window.onload = function () {
         document.documentElement.classList.toggle("is-active");
     });
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
