c9e60ab6c23e715e0cf5074afe52ed1343d345c2
lucyhao <hao.lucyy@gmail.com>
2024-08-12 10:10:35 -0700

    cleanup

--- cv/.auctex-auto/cv.el
+++ cv/.auctex-auto/cv.el
@@ -1,10 +1,12 @@
+;; -*- lexical-binding: t; -*-
+
 (TeX-add-style-hook
  "cv"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "letterpaper" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("geometry" "left=1in" "right=1in" "bottom=0.7in" "top=0.7in") ("xcolor" "dvipsnames") ("newtxmath" "libertine")))
+                     '(("inputenc" "utf8") ("longtable" "") ("graphicx" "") ("hyphenat" "") ("hyperref" "") ("fancyhdr" "") ("ifthen" "") ("everypage" "") ("enumitem" "") ("amsmath" "") ("setspace" "") ("geometry" "left=1in" "right=1in" "bottom=0.7in" "top=0.7in") ("xcolor" "dvipsnames") ("libertine" "") ("newtxmath" "libertine")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
